4f66bb8b6b6f4e4015c1a92dcf352169beee864c
MaiNgoc <mvn.bp01@gmail.com>
2021-12-24 15:58:06 +0700

    Update file

--- 51900147_MaiVanNgoc-51900145_PhamHungNghia-51900200_NguyenThanhSon/www/index.php
+++ 51900147_MaiVanNgoc-51900145_PhamHungNghia-51900200_NguyenThanhSon/www/index.php
@@ -24,30 +24,85 @@
 					</li>
 
 					<li class="nav-item">
-						<a class="nav-link" href="#">Profile</a>
+						<a class="nav-link" href="#">Hồ sơ</a>
 					</li>
 					<li class="nav-item">
 						<a class="nav-link" href="#">Quản lý phòng ban</a>
 					</li>
 					<li class="nav-item">
-						<a class="nav-link" href="#">Logout</a>
+						<a class="nav-link" href="#">Đăng xuất</a>
 					</li>		
 				</ul>
 			</nav>
 
 			<div class="btn-showlist">
-				<button class="btn-list-item">
-					- - -
-				</button>
+				<button class="btn-list-item"></button>
 			</div>
 		</header>
 
 		<div class="body">
-
+			<div class="header-body">
+				<h3 style="margin-bottom:0">Danh sách nhân viên</h3>
+				<button class="add-staff text-white">+ Thêm nhân viên</button>
+				<div class="search">
+					<label for="search-staff" class="search-lable">Tìm kiếm:</label>
+					<input type="search" name="search-staff" id="search-staff" placeholder="Search...">
+				</div>
+			</div>
+			<table id="staff-table" class="table table-striped">
+				<thead>
+					<tr>
+						<th  class="text-center">STT</th>
+						<th  class="text-center">Tên</th>
+						<th  class="text-center">Chức vụ</th>
+						<th  class="text-center">Phòng ban</th>
+						<th  class="text-center">Email</th>
+						<th  class="text-center">Hoạt động</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<td>1</td>
+						<td>Mai Văn Ngọc</td>
+						<td>Trường phòng</td>
+						<td>Kế toán</td>
+						<td>user@gmail.com</td>
+						<td class="list-btn">
+							<div class="btn-view text-white">Xem</div>
+							<div class="btn-edit text-white">Chỉnh sửa</div>
+							<div class="btn-delete text-white">Xóa</div>
+						</td>
+					</tr>
+					<tr>
+						<td>2</td>
+						<td>Phạm Hùng Nghĩa</td>
+						<td>Trường phòng</td>
+						<td>Kế toán</td>
+						<td>user@gmail.com</td>
+						<td class="list-btn">
+						<div class="btn-view text-white">Xem</div>
+							<div class="btn-edit text-white">Chỉnh sửa</div>
+							<div class="btn-delete text-white">Xóa</div>
+						</td>
+					</tr>
+					<tr>
+						<td>3</td>
+						<td>Nguyễn Thanh Sơn</td>
+						<td>Trường phòng</td>
+						<td>Kế toán</td>
+						<td>user@gmail.com</td>
+						<td class="list-btn">
+						<div class="btn-view text-white">Xem</div>
+							<div class="btn-edit text-white">Chỉnh sửa</div>
+							<div class="btn-delete text-white">Xóa</div>
+						</td>
+					</tr>
+				</tbody>
+			</table>
 		</div>
 
 		<footer class="footer">
-
+			
 		</footer>
 	</div>
 
