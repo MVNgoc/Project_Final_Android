c49c2a8e620bc7ebf129436b42925c31d6c08a95
MaiNgoc <mvn.bp01@gmail.com>
2022-01-03 11:39:14 +0700

    update day_off

--- 51900147_MaiVanNgoc-51900145_PhamHungNghia-51900200_NguyenThanhSon/mysql/sql/data.sql
+++ 51900147_MaiVanNgoc-51900145_PhamHungNghia-51900200_NguyenThanhSon/mysql/sql/data.sql
@@ -22,7 +22,7 @@ CREATE TABLE account (
   department_name varchar(64) COLLATE utf8_unicode_ci NOT NULL,
   email varchar(64) COLLATE utf8_unicode_ci NOT NULL,
   phone_number varchar(15) NOT NULL,
-  day_off int(3) NOT NULL,
+  day_off int(3),
   avatar varchar(255) COLLATE utf8_unicode_ci NOT NULL
 );
 
